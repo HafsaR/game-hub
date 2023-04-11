d3dbbaad528ce742dbacf0a835889e16d40ee791
HafsaR <hafsa34343@gmail.com>
2023-04-11 07:07:48 -0600

    show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,46 +3,52 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 export interface Platforms {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platforms}[];
-    metacritic: number
-  }
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platforms }[];
+  metacritic: number;
+}
 
- export interface FetchGameResponse {
-    count: number;
-    results: Game[];
-  }
+export interface FetchGameResponse {
+  count: number;
+  results: Game[];
+}
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setLoading(true);
     apiClient
-      .get<FetchGameResponse>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
-      .catch((err) =>
-     { if (err instanceof CanceledError) return
-      setError(err.message)}
-      );
+      .get<FetchGameResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false)
+      });
 
-      return () => controller.abort()
+    return () => controller.abort();
   }, []);
 
   return {
-    games, error
-  }
+    games,
+    error,
+    isLoading
+  };
+};
 
-}
-
-export default useGames
+export default useGames;
